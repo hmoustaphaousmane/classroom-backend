f2d73be15f552a4e804ef73e5275177d6b691ddf
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2026-03-27 18:03:30 +0100

    implement get all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import cors from "cors";
+
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
+if (!process.env.FRONTEND_URL) throw new Error('FRONTEND_URL is not set in .env file');
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'FRONTEND_URL'],
+  credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/v1/subjects', subjectsRouter)
 
 app.get("/", (_req, res) => {
   res.send("Server is running.");
---
+++ src/routes/subjects.ts
@@ -0,0 +1,77 @@
+import express from "express";
+import {and, desc, eq, getTableColumns, ilike, or, sql} from "drizzle-orm";
+import {departments, subjects} from "../db/schema";
+import {db} from "../db";
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const {search, department, page = 1, limit = 10} = req.query;
+
+        const currentPage = Math.max(1, parseInt(String(page), 10) || 1);
+        const limitPerPage = Math.min(Math.max(1, parseInt(String(limit), 10) || 10), 100); // Max 100 records per page
+
+        const offset = (currentPage - 1) * limitPerPage;
+
+        const filterConditions = [];
+
+        // If the search query exists, filter by subject name OR subject code
+        if (search) {
+            const searchPattern = `$%${String(search).replace(/[%_]/g, '\\%&')}}%`;
+            filterConditions.push(
+                or(
+                    ilike(subjects.name, `%${searchPattern}%`),
+                    ilike(subjects.code, `%${searchPattern}%`)
+                )
+            );
+        }
+
+        // If the department filter exists, match the department name
+        if (department) {
+            // Properly escape department (to safeguard it from SQL injection)
+            const departmentPattern = `$%${String(department).replace(/[%_]/g, '\\%&')}}%`;
+            filterConditions.push(
+                ilike(departments.name, `%${departmentPattern}%`)
+            );
+        }
+
+        // Combine all filters using AND if any exists
+        const whereClause = filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+        const countResult = await db
+            .select({ count: sql<number>`count(*)`})
+            .from(subjects)
+            .leftJoin(departments, eq(subjects.departmentId, departments.id))
+            .where(whereClause);
+
+        const totalCount = countResult[0]?.count ?? 0;
+
+        const subjectsList = await db
+            .select({
+                ...getTableColumns(subjects),
+                department: { ...getTableColumns(departments)}
+            })
+            .from(subjects)
+            .leftJoin(departments, eq(subjects.departmentId, departments.id))
+            .where(whereClause)
+            .orderBy(desc((subjects.createdAt)))
+            .limit(limitPerPage)
+            .offset(offset);
+
+        res.status(200).json({
+            data: subjectsList,
+            pagination: {
+                page: currentPage,
+                limit: limitPerPage,
+                total: totalCount,
+                totalPages: Math.ceil(totalCount / limitPerPage)
+            }
+        })
+    } catch (e) {
+        console.error(`GET /subjects error: ${e}`);
+        res.status(500).json({ error: 'Failed to fetch subjects.'})
+    }
+})
+
+export default router;
